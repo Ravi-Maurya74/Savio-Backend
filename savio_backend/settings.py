b3c304a4f9f829b459e73872eafc67279944781b
Ravi <lcs2020043@iiitl.ac.in>
2023-11-27 17:57:27 +0530

    deploy ready

--- savio_backend/settings.py
+++ savio_backend/settings.py
@@ -30,12 +30,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "os.environ.get(SECRET_KEY)"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+).split(" ")
 
 
 # Application definition
@@ -109,13 +111,12 @@ DATABASES = {
     }
 }
 
-database_url = os.environ.get(
-    "DATABASE_URL", "postgresql://devuser:changeme@localhost:5432/postgres"
-)
+database_url = os.environ.get("DATABASE_URL")
 
 DATABASES["default"] = dj_database_url.parse(database_url)
 
 # postgresql://USER:PASSWORD@HOST:PORT/NAME
+# postgresql://devuser:changeme@localhost:5432/postgres
 
 
 # Password validation
