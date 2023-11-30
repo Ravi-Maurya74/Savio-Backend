9c8dd860dc9e380e8b82a84055979e6c98a6cf94
Ravi <lcs2020043@iiitl.ac.in>
2023-11-30 14:24:30 +0530

    vercel

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
 
 echo "Build Started"
 
-python3.9 -m pip3.9 install --upgrade pip
-python3.9 -m pip3.9 install -r requirements.txt --use-deprecated=legacy-resolver
+# python3.9 -m pip3.9 install --upgrade pip
+pip3.9 install -r requirements.txt --use-deprecated=legacy-resolver
 python3.9 manage.py collectstatic --noinput --clear
 rm requirements.txt
 
